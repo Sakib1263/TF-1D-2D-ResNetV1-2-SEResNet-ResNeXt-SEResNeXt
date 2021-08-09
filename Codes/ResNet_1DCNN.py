0c895a6bc7e0b5597d28bf5340f3b5d6305bcdf9
Sakib Mahmud <sm1512633@qu.edu.qa>
2021-08-09 12:44:22 +0300

    Update ResNet_1DCNN.py

--- Codes/ResNet_1DCNN.py
+++ Codes/ResNet_1DCNN.py
@@ -2,7 +2,7 @@
 """Reference: [Deep Residual Learning for Image Recognition] (https://arxiv.org/abs/1512.03385)"""
 
 from keras.models import Model
-from keras.layers import Input, BatchNormalization, Activation, Add, Dense
+from keras.layers import Input, BatchNormalization, Activation, Add, Dense, Flatten, Dropout
 from keras.layers import Conv1D, MaxPooling1D, GlobalAveragePooling1D, GlobalMaxPooling1D
 
 
@@ -11,7 +11,7 @@ def stem(inputs, num_filters):
     # inputs : input vector
     # First Convolutional layer, where pooled feature maps will be reduced by 75%
     conv = Conv1D(num_filters, 7, strides=2, padding='same', kernel_initializer="he_normal")(inputs)
-    if (conv.shape[1] <= 2):
+    if conv.shape[1] <= 2:
         pool = MaxPooling1D(pool_size=1, strides=2, padding="valid")(conv)
     else:
         pool = MaxPooling1D(pool_size=2, strides=2, padding="valid")(conv)
@@ -57,7 +57,7 @@ def residual_group(inputs, n_filters, n_blocks, conv=True):
     for _ in range(n_blocks):
         out = residual_block(inputs, n_filters)
 
-    # Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+    # Double the size of filters and reduce feature maps by 75% (strides=2) to fit the next Residual Group
     if conv:
         out = conv_block(out, n_filters * 2)
     return out
@@ -86,7 +86,7 @@ def stem_bottleneck(inputs, num_filters):
     # inputs : input vector
     # First Convolutional layer, where pooled feature maps will be reduced by 75%
     conv = Conv1D(num_filters, 7, strides=2, padding='same', kernel_initializer="he_normal")(inputs)
-    if (conv.shape[1] <= 2):
+    if conv.shape[1] <= 2:
         pool = MaxPooling1D(pool_size=1, strides=2, padding="valid")(conv)
     else:
         pool = MaxPooling1D(pool_size=2, strides=2, padding="valid")(conv)
@@ -172,144 +172,101 @@ def learner152(inputs, num_filters):
     return out
 
 
-def classifier(inputs, n_classes, pooling):
+def classifier(inputs, class_number):
     # Construct the Classifier Group
-    # x         : input vector
-    # n_classes : number of output classes
-    # Pool at the end of all the convolutional residual blocks
-    x = []
-    if pooling == 'avg':
-        x = GlobalAveragePooling1D()(inputs)
-    elif pooling == 'max':
-        x = GlobalMaxPooling1D()(inputs)
-    # Final Dense Outputting Layer for the outputs
-    out = Dense(n_classes, activation='softmax')(x)
+    # inputs       : input vector
+    # class_number : number of output classes
+    out = Dense(class_number, activation='softmax')(inputs)
     return out
 
 
-def regressor(inputs, feature_number, pooling):
+def regressor(inputs, feature_number):
     # Construct the Regressor Group
-    # x         : input vector
-    # n_classes : number of output classes
-    # Pool at the end of all the convolutional residual blocks
-    x = []
-    if pooling == 'avg':
-        x = GlobalAveragePooling1D()(inputs)
-    elif pooling == 'max':
-        x = GlobalMaxPooling1D()(inputs)
-    # Final Dense Outputting Layer for the outputs
-    out = Dense(feature_number, activation='linear')(x)
+    # inputs       : input vector
+    # feature_number : number of output features
+    out = Dense(feature_number, activation='linear')(inputs)
     return out
 
 
 class ResNet:
-    def __init__(self, length, num_channel, num_filters, problem_type='Regression', output_nums=1, pooling='avg'):
+    def __init__(self, length, num_channel, num_filters, problem_type='Regression',
+                 output_nums=1, pooling='avg', dropout=False, dropout_rate=0.2):
         self.length = length
         self.num_channel = num_channel
         self.num_filters = num_filters
         self.problem_type = problem_type
         self.output_nums = output_nums
         self.pooling = pooling
+        self.dropout = dropout
+        self.dropout_rate = dropout_rate
+
+    def MLP(self, x):
+        outputs = []
+        if self.pooling == 'avg':
+            x = GlobalAveragePooling1D()(x)
+        elif self.pooling == 'max':
+            x = GlobalMaxPooling1D()(x)
+        # Final Dense Outputting Layer for the outputs
+        x = Flatten(name='flatten')(x)
+        if self.dropout:
+            x = Dropout(self.dropout_rate, name='Dropout')(x)
+        # Problem Types
+        if self.problem_type == 'Classification':
+            # The Classifier for n classes
+            class_number = self.output_nums
+            outputs = classifier(x, class_number)
+        elif self.problem_type == 'Regression':
+            # The Regressor [Default]
+            feature_number = self.output_nums
+            outputs = regressor(x, feature_number)
+        return outputs
+
     def ResNet18(self):
-        # num_filters = 64 [Default]
         outputs = []
         inputs = Input((self.length, self.num_channel))      # The input tensor
         stem_ = stem(inputs, self.num_filters)               # The Stem Convolution Group
         x = learner18(stem_, self.num_filters)               # The learner
-        pooling = self.pooling
-        # Problem Types
-        if self.problem_type == 'Classification':
-            # The Classifier for n classes
-            class_number = self.output_nums
-            outputs = classifier(x, class_number, pooling)
-        elif self.problem_type == 'Regression':
-            # The Regressor [Default]
-            feature_number = self.output_nums
-            outputs = regressor(x, feature_number, pooling)
+        outputs = self.MLP(x)
         # Instantiate the Model
         model = Model(inputs, outputs)
         return model
 
-
     def ResNet34(self):
-        # num_filters = 64 [Default]
         outputs = []
         inputs = Input((self.length, self.num_channel))      # The input tensor
         stem_ = stem(inputs, self.num_filters)               # The Stem Convolution Group
         x = learner34(stem_, self.num_filters)               # The learner
-        pooling = self.pooling
-        # Problem Types
-        if self.problem_type == 'Classification':
-            # The Classifier for n classes
-            class_number = self.output_nums
-            outputs = classifier(x, class_number, pooling)
-        elif self.problem_type == 'Regression':
-            # The Regressor [Default]
-            feature_number = self.output_nums
-            outputs = regressor(x, feature_number, pooling)
+        outputs = self.MLP(x)
         # Instantiate the Model
         model = Model(inputs, outputs)
         return model
 
-
     def ResNet50(self):
-        # num_filters = 64 [Default]
         outputs = []
         inputs = Input((self.length, self.num_channel))     # The input tensor
         stem_b = stem_bottleneck(inputs, self.num_filters)  # The Stem Convolution Group
         x = learner50(stem_b, self.num_filters)             # The learner
-        pooling = self.pooling
-        # Problem Types
-        if self.problem_type == 'Classification':
-            # The Classifier for n classes
-            class_number = self.output_nums
-            outputs = classifier(x, class_number, pooling)
-        elif self.problem_type == 'Regression':
-            # The Regressor [Default]
-            feature_number = self.output_nums
-            outputs = regressor(x, feature_number, pooling)
+        outputs = self.MLP(x)
         # Instantiate the Model
         model = Model(inputs, outputs)
         return model
 
-
     def ResNet101(self):
-        # num_filters = 64 [Default]
         outputs = []
         inputs = Input((self.length, self.num_channel))     # The input tensor
         stem_b = stem_bottleneck(inputs, self.num_filters)  # The Stem Convolution Group
         x = learner101(stem_b, self.num_filters)            # The learner
-        pooling = self.pooling
-        # Problem Types
-        if self.problem_type == 'Classification':
-            # The Classifier for n classes
-            class_number = self.output_nums
-            outputs = classifier(x, class_number, pooling)
-        elif self.problem_type == 'Regression':
-            # The Regressor [Default]
-            feature_number = self.output_nums
-            outputs = regressor(x, feature_number, pooling)
+        outputs = self.MLP(x)
         # Instantiate the Model
         model = Model(inputs, outputs)
         return model
 
-
     def ResNet152(self):
-        # num_filters = 64 [Default]
         outputs = []
         inputs = Input((self.length, self.num_channel))     # The input tensor
         stem_b = stem_bottleneck(inputs, self.num_filters)  # The Stem Convolution Group
         x = learner152(stem_b, self.num_filters)            # The learner
-        pooling = self.pooling
-        # Problem Types
-        if self.problem_type == 'Classification':
-            # The Classifier for n classes
-            class_number = self.output_nums
-            outputs = classifier(x, class_number, pooling)
-        elif self.problem_type == 'Regression':
-            # The Regressor [Default]
-            feature_number = self.output_nums
-            outputs = regressor(x, feature_number, pooling)
+        outputs = self.MLP(x)
         # Instantiate the Model
         model = Model(inputs, outputs)
         return model
