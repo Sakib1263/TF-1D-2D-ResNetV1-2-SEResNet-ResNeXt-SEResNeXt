72ed69ffeb44d3706bf7a1ef5f0f18fd281cd87a
Sakib Mahmud <sakib.mahmud@qu.edu.qa>
2021-11-25 14:09:11 +0300

    Add files via upload

---
+++ Codes/ResNet_v2_1DCNN.py
@@ -0,0 +1,278 @@
+import tensorflow as tf
+
+
+def Conv_1D_Block(x, model_width, kernel, strides):
+    # 1D Convolutional Block with BatchNormalization
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+    x = tf.keras.layers.Conv1D(model_width, kernel, strides=strides, padding="same", kernel_initializer="he_normal")(x)
+
+    return x
+
+
+def stem(inputs, num_filters):
+    # Construct the Stem Convolution Group
+    # inputs : input vector
+    # First Convolutional layer, where pooled feature maps will be reduced by 75%
+    conv = Conv_1D_Block(inputs, num_filters, 7, 2)
+    if conv.shape[1] <= 2:
+        pool = tf.keras.layers.MaxPooling1D(pool_size=1, strides=2, padding="valid")(conv)
+    else:
+        pool = tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding="valid")(conv)
+    return pool
+
+
+def conv_block(inputs, num_filters):
+    # Construct Block of Convolutions without Pooling
+    # x        : input into the block
+    # n_filters: number of filters
+    conv = Conv_1D_Block(inputs, num_filters, 3, 2)
+    conv = Conv_1D_Block(conv, num_filters, 3, 2)
+    return conv
+
+
+def residual_block(inputs, num_filters):
+    # Construct a Residual Block of Convolutions
+    # x        : input into the block
+    # n_filters: number of filters
+    shortcut = inputs
+    #
+    conv = Conv_1D_Block(inputs, num_filters, 3, 1)
+    conv = Conv_1D_Block(conv, num_filters, 3, 1)
+    conv = tf.keras.layers.Add()([conv, shortcut])
+    out = tf.keras.layers.Activation('relu')(conv)
+    return out
+
+
+def residual_group(inputs, num_filters, n_blocks, conv=True):
+    # x        : input to the group
+    # n_filters: number of filters
+    # n_blocks : number of blocks in the group
+    # conv     : flag to include the convolution block connector
+    out = inputs
+    for i in range(n_blocks):
+        out = residual_block(out, num_filters)
+
+    # Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+    if conv:
+        out = conv_block(out, num_filters * 2)
+    return out
+
+
+def stem_bottleneck(inputs, num_filters):
+    # Construct the Stem Convolution Group
+    # inputs : input vector
+    # First Convolutional layer, where pooled feature maps will be reduced by 75%
+    conv = Conv_1D_Block(inputs, num_filters, 7, 2)
+    if conv.shape[1] <= 2:
+        pool = tf.keras.layers.MaxPooling1D(pool_size=1, strides=2, padding="valid")(conv)
+    else:
+        pool = tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding="valid")(conv)
+    return pool
+
+
+def conv_block_bottleneck(inputs, num_filters):
+    # Construct Block of Convolutions without Pooling
+    # x        : input into the block
+    # n_filters: number of filters
+    conv = Conv_1D_Block(inputs, num_filters, 3, 2)
+    conv = Conv_1D_Block(conv, num_filters, 3, 2)
+    conv = Conv_1D_Block(conv, num_filters, 3, 2)
+    return conv
+
+
+def residual_block_bottleneck(inputs, num_filters):
+    # Construct a Residual Block of Convolutions
+    # x        : input into the block
+    # n_filters: number of filters
+    shortcut = Conv_1D_Block(inputs, num_filters * 4, 1, 1)
+    #
+    conv = Conv_1D_Block(inputs, num_filters, 1, 1)
+    conv = Conv_1D_Block(conv, num_filters, 3, 1)
+    conv = Conv_1D_Block(conv, num_filters * 4, 1, 1)
+    conv = tf.keras.layers.Add()([conv, shortcut])
+    out = tf.keras.layers.Activation('relu')(conv)
+
+    return out
+
+
+def residual_group_bottleneck(inputs, num_filters, n_blocks, conv=True):
+    # x        : input to the group
+    # n_filters: number of filters
+    # n_blocks : number of blocks in the group
+    # conv     : flag to include the convolution block connector
+    out = inputs
+    for i in range(n_blocks):
+        out = residual_block_bottleneck(out, num_filters)
+
+    # Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+    if conv:
+        out = conv_block_bottleneck(out, num_filters * 2)
+
+    return out
+
+
+def learner18(inputs, num_filters):
+    # Construct the Learner
+    x = residual_group(inputs, num_filters, 2)          # First Residual Block Group of 64 filters
+    x = residual_group(x, num_filters * 2, 1)           # Second Residual Block Group of 128 filters
+    x = residual_group(x, num_filters * 4, 1)           # Third Residual Block Group of 256 filters
+    out = residual_group(x, num_filters * 8, 1, False)  # Fourth Residual Block Group of 512 filters
+    return out
+
+
+def learner34(inputs, num_filters):
+    # Construct the Learner
+    x = residual_group(inputs, num_filters, 3)          # First Residual Block Group of 64 filters
+    x = residual_group(x, num_filters * 2, 3)           # Second Residual Block Group of 128 filters
+    x = residual_group(x, num_filters * 4, 5)           # Third Residual Block Group of 256 filters
+    out = residual_group(x, num_filters * 8, 2, False)  # Fourth Residual Block Group of 512 filters
+    return out
+
+
+def learner50(inputs, num_filters):
+    # Construct the Learner
+    x = residual_group_bottleneck(inputs, num_filters, 3)  # First Residual Block Group of 64 filters
+    x = residual_group_bottleneck(x, num_filters * 2, 3)   # Second Residual Block Group of 128 filters
+    x = residual_group_bottleneck(x, num_filters * 4, 5)   # Third Residual Block Group of 256 filters
+    out = residual_group_bottleneck(x, num_filters * 8, 2, False)  # Fourth Residual Block Group of 512 filters
+    return out
+
+
+def learner101(inputs, num_filters):
+    # Construct the Learner
+    x = residual_group_bottleneck(inputs, num_filters, 3)  # First Residual Block Group of 64 filters
+    x = residual_group_bottleneck(x, num_filters * 2, 3)   # Second Residual Block Group of 128 filters
+    x = residual_group_bottleneck(x, num_filters * 4, 22)  # Third Residual Block Group of 256 filters
+    out = residual_group_bottleneck(x, num_filters * 8, 2, False)  # Fourth Residual Block Group of 512 filters
+    return out
+
+
+def learner152(inputs, num_filters):
+    # Construct the Learner
+    x = residual_group_bottleneck(inputs, num_filters, 3)  # First Residual Block Group of 64 filters
+    x = residual_group_bottleneck(x, num_filters * 2, 7)   # Second Residual Block Group of 128 filters
+    x = residual_group_bottleneck(x, num_filters * 4, 35)  # Third Residual Block Group of 256 filters
+    out = residual_group_bottleneck(x, num_filters * 8, 2, False)  # Fourth Residual Block Group of 512 filters
+    return out
+
+
+def classifier(inputs, class_number):
+    # Construct the Classifier Group
+    # inputs       : input vector
+    # class_number : number of output classes
+    out = tf.keras.layers.Dense(class_number, activation='softmax')(inputs)
+    return out
+
+
+def regressor(inputs, feature_number):
+    # Construct the Regressor Group
+    # inputs       : input vector
+    # feature_number : number of output features
+    out = tf.keras.layers.Dense(feature_number, activation='linear')(inputs)
+    return out
+
+
+class ResNetv2:
+    def __init__(self, length, num_channel, num_filters, problem_type='Regression',
+                 output_nums=1, pooling='avg', dropout_rate=False):
+        self.length = length
+        self.num_channel = num_channel
+        self.num_filters = num_filters
+        self.problem_type = problem_type
+        self.output_nums = output_nums
+        self.pooling = pooling
+        self.dropout_rate = dropout_rate
+
+    def MLP(self, x):
+        if self.pooling == 'avg':
+            x = tf.keras.layers.GlobalAveragePooling1D()(x)
+        elif self.pooling == 'max':
+            x = tf.keras.layers.GlobalMaxPooling1D()(x)
+        # Final Dense Outputting Layer for the outputs
+        x = tf.keras.layers.Flatten(name='flatten')(x)
+        if self.dropout_rate:
+            x = tf.keras.layers.Dropout(self.dropout_rate, name='Dropout')(x)
+        outputs = tf.keras.layers.Dense(self.output_nums, activation='linear')(x)
+        if self.problem_type == 'Classification':
+            outputs = tf.keras.layers.Dense(self.output_nums, activation='softmax')(x)
+
+        return outputs
+
+    def ResNet18(self):
+        inputs = tf.keras.Input((self.length, self.num_channel))      # The input tensor
+        stem_ = stem(inputs, self.num_filters)               # The Stem Convolution Group
+        x = learner18(stem_, self.num_filters)               # The learner
+        outputs = self.MLP(x)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+    def ResNet34(self):
+        inputs = tf.keras.Input((self.length, self.num_channel))      # The input tensor
+        stem_ = stem(inputs, self.num_filters)               # The Stem Convolution Group
+        x = learner34(stem_, self.num_filters)               # The learner
+        outputs = self.MLP(x)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+    def ResNet50(self):
+        inputs = tf.keras.Input((self.length, self.num_channel))     # The input tensor
+        stem_b = stem_bottleneck(inputs, self.num_filters)  # The Stem Convolution Group
+        x = learner50(stem_b, self.num_filters)             # The learner
+        outputs = self.MLP(x)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+    def ResNet101(self):
+        inputs = tf.keras.Input((self.length, self.num_channel))     # The input tensor
+        stem_b = stem_bottleneck(inputs, self.num_filters)  # The Stem Convolution Group
+        x = learner101(stem_b, self.num_filters)            # The learner
+        outputs = self.MLP(x)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+    def ResNet152(self):
+        inputs = tf.keras.Input((self.length, self.num_channel))     # The input tensor
+        stem_b = stem_bottleneck(inputs, self.num_filters)  # The Stem Convolution Group
+        x = learner152(stem_b, self.num_filters)            # The learner
+        outputs = self.MLP(x)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+
+if __name__ == '__main__':
+    # Configurations
+    length = 1024  # Length of each Segment
+    model_name = 'ResNet152'  # DenseNet Models
+    model_width = 16 # Width of the Initial Layer, subsequent layers start from here
+    num_channel = 1  # Number of Input Channels in the Model
+    problem_type = 'Regression' # Classification or Regression
+    output_nums = 1  # Number of Class for Classification Problems, always '1' for Regression Problems
+    #
+    Model = ResNetv2(length, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, pooling='avg', dropout_rate=False).ResNet152()
+    Model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0003), loss=tf.keras.losses.MeanAbsoluteError(), metrics=tf.keras.metrics.MeanSquaredError())
+    Model.summary()
+
+
+if __name__ == '__main__':
+    # Configurations
+    length = 1024  # Length of each Segment
+    model_name = 'ResNet152'  # DenseNet Models
+    model_width = 16 # Width of the Initial Layer, subsequent layers start from here
+    num_channel = 1  # Number of Input Channels in the Model
+    problem_type = 'Regression' # Classification or Regression
+    output_nums = 1  # Number of Class for Classification Problems, always '1' for Regression Problems
+    #
+    Model = ResNetv2(length, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, pooling='avg', dropout_rate=False).ResNet152()
+    Model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0003), loss=tf.keras.losses.MeanAbsoluteError(), metrics=tf.keras.metrics.MeanSquaredError())
+    Model.summary()
